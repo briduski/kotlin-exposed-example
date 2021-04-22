7987b1973cc5aff966401ee386944e3ea6a065cb
briduski <rituski@gmail.com>
2021-04-22 15:34:55 +0200

    Add innejoin example

--- src/main/kotlin/org/briduski/kotlinexample/dao/ExampleDAO.kt
+++ src/main/kotlin/org/briduski/kotlinexample/dao/ExampleDAO.kt
@@ -2,64 +2,113 @@ package org.briduski.kotlinexample.dao
 
 import org.briduski.kotlinexample.dao.model.Author
 import org.briduski.kotlinexample.dao.model.Course
+import org.briduski.kotlinexample.dao.model.CourseTopic
 import org.briduski.kotlinexample.dao.tables.Authors
 import org.briduski.kotlinexample.dao.tables.Courses
+import org.briduski.kotlinexample.dao.tables.CoursesTopic
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.time.Duration
 
 fun main() {
-//    Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver", user = "root", password = "")
-    Database.connect("jdbc:postgresql://localhost:7878/postgres", driver = "org.postgresql.Driver", user = "postgres", password = "postgres")
+    Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver", user = "root", password = "")
+//    Database.connect("jdbc:postgresql://localhost:7878/postgres", driver = "org.postgresql.Driver", user = "postgres", password = "postgres")
     transaction {
-        SchemaUtils.drop (Courses, Authors)
+        SchemaUtils.drop (Courses, Authors, CoursesTopic)
     }
 
     transaction {
-        addLogger(StdOutSqlLogger)
+     //   addLogger(StdOutSqlLogger)
 
-        SchemaUtils.create(Courses, Authors)
+        SchemaUtils.create(CoursesTopic,Courses, Authors )
+        val topic1 = CourseTopic.new {
+            course_topic = "Kafka"
+            topic_id = 100
+        }
+       val topic2 = CourseTopic.new {
+            course_topic = "Java"
+            topic_id = 200
+        }
         val author1 = Author.new {
             name = "Stephan M."
-            rate = 10
+            rate = 5454
         }
         val author2 = Author.new {
             name = "Dilip D."
-            rate = 8
+            rate = 8787
         }
-        val course1 = Course.new {
+       val course1 = Course.new {
             name = "Kafka Streams Introduction"
             duration = Duration.ofHours(5L).toHours()
-            topic = "Kafka"
+            topic_id = 100
             author = author1
         }
         val course2 = Course.new {
             name = "Kafka Connect - Advanced concepts"
             duration = Duration.ofHours(9L).toHours()
-            topic = "Kafka"
+            topic_id = 100
             author = author1
         }
         val course3 = Course.new {
             name = "Spring Complete"
             duration = Duration.ofHours(9L).toHours()
-            topic = "Java"
+            topic_id = 200
             author = author2
         }
+        println("\n * * * * * * * * ")
+        println("-> All Authors:")
 
+        for (author in Authors.selectAll()) {
+            println(" ->  id: ${author[Authors.id]}: " + " name: ${author[Authors.name]}+  rate: ${author[Authors.rate]}")
+        }
+        println("\n * * * * * * * * ")
+        println("-> All CoursesTopics:")
+
+        for (courseTopic in CoursesTopic.selectAll()) {
+            println(" ->  ${courseTopic[CoursesTopic.topic_id]}:" +   " ${courseTopic[CoursesTopic.course_topic]}")
+        }
+
+        println("\n * * * * * * * *")
         println("-> All courses:")
 
         for (course in Courses.selectAll()) {
-            println(" ->  ${course[Courses.name]}:" +   " ${course[Courses.topic]}:" +
-                    " ${course[Courses.id]}:" +   " ${course[Courses.author]}:")
+            println(" -> courseName: ${course[Courses.name]}:" +   ", courseTopicId: ${course[Courses.topic]}:" +
+                    ", courseId:  ${course[Courses.id]}:" +   ",  authorId: ${course[Courses.author]}:")
         }
 
-        println(" * * * * * * * * ")
-        println("<><><>  Authors: ${Author.all().joinToString {it.name}}")
-        println("<><><> Courses from author ${author1.name}: => " +
-                "${author1.authors.joinToString {it.name}}")
+//        println(" * * * * * * * * \n")
+//        println("<><><>  Authors: ${Author.all().joinToString {it.name}}")
+//        println("<><><> Courses from author ${author1.name}: => " +
+//                "${author1.authors.joinToString {it.name}}")
+//
+//        println(" * * * * * * * * \n")
+//        println("\n== == Extended courses: ${Course.find { Courses.duration greaterEq 6 }.joinToString {it.name}}")
 
-        println("== == Extended courses: ${Course.find { Courses.duration greaterEq 6 }.joinToString {it.name}}")
-        println(" * * * * * * * * ")
+        println("\n * * * * * * * * ")
+        val userTable1 = CoursesTopic.alias("1")
+//        public fun <C1 : org.jetbrains.exposed.sql.ColumnSet, C2 : org.jetbrains.exposed.sql.ColumnSet>
+//                C1.innerJoin(otherTable: C2,
+//                                onColumn: C1.() -> org.jetbrains.exposed.sql.Expression<*>,
+//                             otherColumn: C2.() -> org.jetbrains.exposed.sql.Expression<*>)
+//
+//                : org.jetbrains.exposed.sql.Join { /* compiled code */ }
+
+         Courses
+                .innerJoin(userTable1 , {Courses.topic}, {userTable1[CoursesTopic.topic_id]} )
+//                .innerJoin(userTable1 , {Courses.topic}, {CoursesTopic.topic_id} ) /// error!!!
+                .selectAll()
+                .map {
+                    println("${it[Courses.name]}" +
+                            " - course_id-orig: ${it[Courses.topic]} " +
+                            " - course_id-ref: ${it[userTable1[CoursesTopic.topic_id]]} " +
+                            " - course_name-ref: ${it[userTable1[CoursesTopic.course_topic]]} ")
+                }
+
+
+
+
+
+
     }
 }
 /*
